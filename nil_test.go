68596bbf739ecdd6929c7ce93c3f59bbdf30113d
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-09-30 06:55:34 +0200

    Added nil assertions

---
+++ nil_test.go
@@ -0,0 +1,31 @@
+package assert_test
+
+import (
+    "testing"
+
+    "go.flow.arcalot.io/assert"
+)
+
+func TestNil(t *testing.T) {
+    type testStruct struct{}
+    var v *testStruct
+    assert.Nil(t, nil)
+    assert.Nil(t, v)
+
+    testFailure(t, func(t *testing.T) {
+        assert.Nil(t, "")
+    })
+    testFailure(t, func(t *testing.T) {
+        assert.Nil(t, &testStruct{})
+    })
+}
+
+func TestNotNil(t *testing.T) {
+    type testStruct struct{}
+    var v = &testStruct{}
+    assert.NotNil(t, v)
+    assert.NotNil(t, "Hello world!")
+    testFailure(t, func(t *testing.T) {
+        assert.NotNil(t, nil)
+    })
+}
