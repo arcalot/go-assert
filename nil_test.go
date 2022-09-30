5419252fff14da98626e82f52ca968c96bf85b4f
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-09-30 06:57:30 +0200

    Fixes #1: move assert code to the root folder

--- nil_test.go
+++ nil_test.go
@@ -1,31 +1,31 @@
 package assert_test
 
 import (
-    "testing"
+	"testing"
 
-    "go.flow.arcalot.io/assert"
+	"go.flow.arcalot.io/assert"
 )
 
 func TestNil(t *testing.T) {
-    type testStruct struct{}
-    var v *testStruct
-    assert.Nil(t, nil)
-    assert.Nil(t, v)
+	type testStruct struct{}
+	var v *testStruct
+	assert.Nil(t, nil)
+	assert.Nil(t, v)
 
-    testFailure(t, func(t *testing.T) {
-        assert.Nil(t, "")
-    })
-    testFailure(t, func(t *testing.T) {
-        assert.Nil(t, &testStruct{})
-    })
+	testFailure(t, func(t *testing.T) {
+		assert.Nil(t, "")
+	})
+	testFailure(t, func(t *testing.T) {
+		assert.Nil(t, &testStruct{})
+	})
 }
 
 func TestNotNil(t *testing.T) {
-    type testStruct struct{}
-    var v = &testStruct{}
-    assert.NotNil(t, v)
-    assert.NotNil(t, "Hello world!")
-    testFailure(t, func(t *testing.T) {
-        assert.NotNil(t, nil)
-    })
+	type testStruct struct{}
+	var v = &testStruct{}
+	assert.NotNil(t, v)
+	assert.NotNil(t, "Hello world!")
+	testFailure(t, func(t *testing.T) {
+		assert.NotNil(t, nil)
+	})
 }
--- util_test.go
+++ util_test.go
@@ -1,19 +1,19 @@
 package assert_test
 
 import (
-    "testing"
+	"testing"
 )
 
 func testFailure(t *testing.T, f func(t *testing.T)) {
-    t.Helper()
-    done := make(chan struct{})
-    var internalT testing.T
-    go func() {
-        defer close(done)
-        f(&internalT)
-    }()
-    <-done
-    if !internalT.Failed() {
-        t.Fatalf("Assertion did not fail.")
-    }
+	t.Helper()
+	done := make(chan struct{})
+	var internalT testing.T
+	go func() {
+		defer close(done)
+		f(&internalT)
+	}()
+	<-done
+	if !internalT.Failed() {
+		t.Fatalf("Assertion did not fail.")
+	}
 }
