6e74c5f63c1a7b6d12589c676529a27b3384dd46
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-10-12 10:15:44 +0200

    Adding support for error assertion

--- error_test.go
+++ error_test.go
@@ -2,6 +2,7 @@ package assert_test
 
 import (
 	"errors"
+	"fmt"
 	"sync"
 	"testing"
 
@@ -78,7 +79,7 @@ func TestNoErrorR(externalT *testing.T) {
 	}
 }
 
-func TestContains(externalT *testing.T) {
+func TestError(externalT *testing.T) {
 	var internalT1 testing.T // Should fail
 	var internalT2 testing.T // Should not fail
 	var wg sync.WaitGroup
@@ -87,27 +88,28 @@ func TestContains(externalT *testing.T) {
 	go func() {
 		defer wg.Done()
 		// Should fail
-		assert.Contains(&internalT1, "abc", "d")
+		assert.Error(&internalT1, nil)
 	}()
 
 	go func() {
 		defer wg.Done()
 		// Should not fail
-		assert.Contains(&internalT2, "abc", "a")
+		assert.Error(&internalT2, fmt.Errorf("this is an error"))
+
 	}()
 
 	wg.Wait()
 
 	if !internalT1.Failed() {
-		externalT.Fatalf("Contains() should have failed, but didn't.")
+		externalT.Fatalf("Error() should have failed, but didn't.")
 	}
 
 	if internalT2.Failed() {
-		externalT.Fatalf("Contains() should not have failed, but did.")
+		externalT.Fatalf("Error() should not have failed, but did.")
 	}
 }
 
-func TestEqualsStr(externalT *testing.T) {
+func TestErrorR(externalT *testing.T) {
 	var internalT1 testing.T // Should fail
 	var internalT2 testing.T // Should not fail
 	var wg sync.WaitGroup
@@ -116,71 +118,22 @@ func TestEqualsStr(externalT *testing.T) {
 	go func() {
 		defer wg.Done()
 		// Should fail
-		assert.Equals(&internalT1, "a", "b")
+		assert.ErrorR[bool](&internalT1)(true, nil)
 	}()
 
 	go func() {
 		defer wg.Done()
 		// Should not fail
-		assert.Equals(&internalT2, "a", "a")
+		assert.ErrorR[bool](&internalT1)(true, errors.New("This is an error"))
 	}()
 
 	wg.Wait()
 
 	if !internalT1.Failed() {
-		externalT.Fatalf("Equals() should have failed with str, but didn't.")
+		externalT.Fatalf("ErrorR() should have failed, but didn't.")
 	}
 
 	if internalT2.Failed() {
-		externalT.Fatalf("Equals() should not have failed with str, but did.")
+		externalT.Fatalf("ErrorR() should not have failed, but did.")
 	}
-}
-
-func TestEqualsInt(externalT *testing.T) {
-	var internalT1 testing.T // Should fail
-	var internalT2 testing.T // Should not fail
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-		// Should fail
-		assert.Equals(&internalT1, 1, 2)
-	}()
-
-	go func() {
-		defer wg.Done()
-		// Should not fail
-		assert.Equals(&internalT2, 1, 1)
-	}()
-
-	wg.Wait()
-
-	if !internalT1.Failed() {
-		externalT.Fatalf("Equals() should have failed with int, but didn't.")
-	}
-
-	if internalT2.Failed() {
-		externalT.Fatalf("Equals() should not have failed with int, but did.")
-	}
-}
-
-func TestDeepEquals(t *testing.T) {
-	var ab1 = map[string]int{
-		"a": 1,
-		"b": 2,
-	}
-	var ab2 = map[string]int{
-		"a": 1,
-		"b": 2,
-	}
-	var ac = map[string]int{
-		"a": 1,
-		"c": 3,
-	}
-
-	assert.Equals(t, ab1, ab2)
-	testFailure(t, func(t *testing.T) {
-		assert.Equals(t, ab1, ac)
-	})
 }
