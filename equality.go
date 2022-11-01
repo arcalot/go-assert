b1031df78fa39f5493416c300e3784d9011161a4
jaredoconnell <joconnel@redhat.com>
2022-11-01 13:12:45 -0400

    Improved error message for InstanceOf assertion

--- equality.go
+++ equality.go
@@ -29,6 +29,6 @@ func Equals[T any](t *testing.T, got T, expected T) {
 func InstanceOf[T any](t *testing.T, got any) {
 	_, ok := got.(T)
 	if !ok {
-		t.Fatalf("InstanceOf assertion failed. %v is instance of %T", got, got)
+		t.Fatalf("Bad instance. Tested value %v is instance of %T, not %T", got, got, *new(T))
 	}
 }
