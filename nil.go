2db881348194a57f9bc3b1b11c0fb14e1959caf4
Jared O'Connell <46976761+jaredoconnell@users.noreply.github.com>
2024-01-05 16:54:26 -0500

    Fixed edge cases for Nil and NotNil (#21)
    * Fixed edge cases for Nil and NotNil
    
    * Move to any interface

--- nil.go
+++ nil.go
@@ -8,8 +8,7 @@ import (
 // Nil tests if the provided value is nil. If it is not nil, the assertion fails.
 func Nil(t *testing.T, value any) {
 	t.Helper()
-	v := reflect.ValueOf(value)
-	if v.IsValid() && (v.Kind() != reflect.Pointer || !v.IsNil()) {
+	if !isNil(value) {
 		t.Fatalf("Unexpected non-nil value: %v", value)
 	}
 }
@@ -17,8 +16,7 @@ func Nil(t *testing.T, value any) {
 // NotNil tests that the provided value is not nil. If the value is nil, the assertion fails.
 func NotNil(t *testing.T, value any) {
 	t.Helper()
-	v := reflect.ValueOf(value)
-	if !v.IsValid() || v.Kind() == reflect.Pointer && reflect.ValueOf(value).IsNil() {
+	if isNil(value) {
 		t.Fatalf("Unexpected nil value")
 	}
 }
@@ -26,8 +24,24 @@ func NotNil(t *testing.T, value any) {
 // NotNilR asserts that the input is not nil, and returns it.
 func NotNilR[T any](t *testing.T, value T) T {
 	t.Helper()
-	if reflect.ValueOf(value).IsNil() {
+	if isNil(value) {
 		t.Fatalf("Unexpected nil value")
 	}
 	return value
+}
+
+func isNil(value any) bool {
+	if value == nil {
+		return true
+	}
+	v := reflect.ValueOf(value)
+	nillableKinds := []reflect.Kind{
+		reflect.Pointer, reflect.Chan, reflect.Func, reflect.Interface, reflect.Map,
+		reflect.Ptr, reflect.Slice, reflect.UnsafePointer,
+	}
+	// Must be a valid value, and must be one that can be nil. If it cannot be nil, then it's not nil.
+	if !v.IsValid() || !containsShallow(nillableKinds, v.Kind()) {
+		return false
+	}
+	return v.IsNil()
 }
