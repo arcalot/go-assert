aca3881a427086d84975ff592a13751e7d602677
Jared O'Connell <joconnel@redhat.com>
2024-02-15 16:17:35 -0500

    Added assertions to validate the value of a panic

--- panic.go
+++ panic.go
@@ -16,6 +16,53 @@ func Panics(t *testing.T, functionToTest func()) {
 	functionToTest()
 }
 
+// PanicsContains asserts that the function causes a panic.
+// It also asserts that the panic's error's string representation has
+// the given substring within it.
+// Does not detect panics in separate goroutines.
+func PanicsContains(t *testing.T, functionToTest func(), expectedErrMsg string) {
+	defer func() {
+		// This deferred function will recover it if it panics
+		r := recover()
+		if r == nil {
+			t.Fatalf("Expected function to panic, but it didn't.")
+		}
+		err, isErr := r.(error)
+		str, isStr := r.(string)
+		if !isErr && !isStr {
+			t.Fatalf("panic returned value that is neither a string nor an error; got %T", r)
+		}
+		if !isStr {
+			str = err.Error()
+		}
+		Contains(t, str, expectedErrMsg)
+	}()
+
+	// Run function to see if it panics.
+	functionToTest()
+}
+
+// PanicsWithValidation asserts that the function causes a panic.
+// It also calls the function passed in to allow validation of the panicked value.
+// Does not detect panics in separate goroutines.
+func PanicsWithValidation(
+	t *testing.T,
+	functionToTest func(),
+	validationFunction func(*testing.T, any),
+) {
+	defer func() {
+		// This deferred function will recover it if it panics
+		r := recover()
+		if r == nil {
+			t.Fatalf("Expected function to panic, but it didn't.")
+		}
+		validationFunction(t, r)
+	}()
+
+	// Run function to see if it panics.
+	functionToTest()
+}
+
 // NoPanic asserts that the function does not cause a panic.
 // Does not detect panics in separate goroutines.
 func NoPanic(t *testing.T, functionToTest func()) {
